d20b881bf615ebf320fe1e6ed373b43061b5d7fe
eran0926 <eran0926@users.noreply.github.com>
2025-02-19 10:28:25 +0000

    Deploying to gh-pages from @ eran0926/FRC6998_2025_scouting_app_v0@58c9c458fa73fc94003d3a9bd23d96663fc85cc7 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1245137524"
+    serviceWorkerVersion: "2889672484"
   }
 });
